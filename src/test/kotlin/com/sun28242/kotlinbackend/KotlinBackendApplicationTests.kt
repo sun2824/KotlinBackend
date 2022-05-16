2d58fe939531db4e99890f0240e1e86e7a129580
ronnie <leeyounghoon2824>
2022-05-17 00:32:20 +0900

    기본 템플릿 생성

---
+++ src/test/kotlin/com/sun28242/kotlinbackend/KotlinBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sun28242.kotlinbackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
