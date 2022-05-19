a31c1a6bb24c24d121e833426ccaa226747fa369
ronnie <leeyounghoon2824>
2022-05-20 01:15:42 +0900

    dataSource 설정 완료

--- src/main/kotlin/com/dudgns2824/kotlinbackend/config/datasource/PurchaseDatasourceConfig.kt
+++ src/main/kotlin/com/dudgns2824/kotlinbackend/config/datasource/PurchaseDatasourceConfig.kt
@@ -1,12 +1,15 @@
 package com.dudgns2824.kotlinbackend.config.datasource
 
 import com.zaxxer.hikari.HikariDataSource
-import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.jdbc.DataSourceBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Primary
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean
 import org.springframework.transaction.annotation.EnableTransactionManagement
 import javax.sql.DataSource
 
@@ -19,13 +22,25 @@ import javax.sql.DataSource
 )
 class PurchaseDatasourceConfig {
 
-    @Bean(name = ["purchaseDataSource"])
-    @ConfigurationProperties(prefix = "purchase.datasource.hikari")
-    fun purchaseDatasource() : DataSource {
-        println("데이터 소스 설정")
+    @Bean(name = ["purchaseDataSource"]) // 데이터 소스 빈 등록
+    @ConfigurationProperties(prefix = "purchase.datasource.hikari") // purchase.yml에서 purchase:datasource:hikari 내부의 설정 가져옴
+    fun purchaseDatasource(): DataSource {
         return DataSourceBuilder
             .create()
             .type(HikariDataSource::class.java)
+            .build()
+    }
+
+    @Primary
+    @Bean(name = ["purchaseEntityManagerFactory"])
+    fun purchaseEntityManager(
+        builder: EntityManagerFactoryBuilder,
+        @Qualifier("purchaseDataSource") dataSource: DataSource
+    ): LocalContainerEntityManagerFactoryBean {
+        return builder
+            .dataSource(dataSource)
+            .packages("com.dudgns2824.kotlinbackend.purchase.entity")
+            .persistenceUnit("purchase")
             .build()
     }
 
--- src/main/kotlin/com/dudgns2824/kotlinbackend/purchase/controller/MainController.kt
+++ src/main/kotlin/com/dudgns2824/kotlinbackend/purchase/controller/MainController.kt
@@ -3,6 +3,6 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping
+@RequestMapping("/api/")
 class MainController {
 }
